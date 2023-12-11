046d2379c88d4529b605eaf9efa26a2e1d27a05b
Bilal Bas <bilal.bas@gmx.de>
2023-12-11 13:15:53 +0100

    Add `FeedPresenter` holding feed loading presentation logic (loading state and loaded feed)

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -5,11 +5,12 @@ public final class FeedUIComposer {
     private init() { }
 
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let feedViewModel = FeedViewModel(feedLoader: feedLoader)
-        let refreshController = FeedRefreshViewController(viewModel: feedViewModel)
+        let presenter = FeedPresenter(feedLoader: feedLoader)
+        let refreshController = FeedRefreshViewController(presenter: presenter)
         let feedController = FeedViewController(refreshController: refreshController)
-        
-        feedViewModel.onFeedLoad = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
+
+        presenter.loadingView = refreshController
+        presenter.feedView = FeedViewAdapter(controller: feedController, loader: imageLoader)
         
         return feedController
     }
@@ -20,6 +21,23 @@ public final class FeedUIComposer {
                 let feedImageViewModel = FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init)
                 return FeedImageCellController(viewModel: feedImageViewModel)
             }
+        }
+    }
+}
+
+private final class FeedViewAdapter: FeedView {
+    private weak var controller: FeedViewController?
+    private let loader: FeedImageDataLoader
+    
+    init(controller: FeedViewController, loader: FeedImageDataLoader) {
+        self.controller = controller
+        self.loader = loader
+    }
+
+    func display(feed: [FeedImage]) {
+        controller?.tableModel = feed.map { model in
+            let feedImageViewModel = FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init)
+            return FeedImageCellController(viewModel: feedImageViewModel)
         }
     }
 }
