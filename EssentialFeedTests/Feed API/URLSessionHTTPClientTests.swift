55b34112e031fe6ca47e2fd7c5ed420f94580e6c
Bilal Bas <bilal.bas@gmx.de>
2023-12-15 18:11:00 +0100

    Remove default URLSessionHTTPClient initializer parameter (dependency) as clients should be mindful about which URLSession instance to use. For example, clients may decide to use the .shared, but when testing we can inject as URLSession with an ephmeral configuration registered with the URLProtocolStub class. This way, we don't need to start/stop intercepting URL requests globally

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -3,12 +3,10 @@ import EssentialFeed
 
 final class URLSessionHTTPClientTests: XCTestCase {
     
-    override func setUp() {
-        URLProtocolStub.startInterceptingRequests()
-    }
-    
     override func tearDown() {
-        URLProtocolStub.stopInterceptingRequests()
+        super.tearDown()
+        
+        URLProtocolStub.removeStub()
     }
     
     func test_getFromURL_performsGETRequestWithURL() {
@@ -78,10 +76,12 @@ final class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
-        let sut = URLSessionHTTPClient()
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
         
+        let sut = URLSessionHTTPClient(session: session)
         trackForMemoryLeaks(sut, file: file, line: line)
-
         return sut
     }
     
@@ -163,12 +163,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
         }
         
-        static func startInterceptingRequests() {
-            URLProtocol.registerClass(self)
-        }
-        
-        static func stopInterceptingRequests() {
-            URLProtocol.unregisterClass(self)
+        static func removeStub() {
             stub = nil
         }
         
