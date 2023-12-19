e726a7263e485458165c581e5e8ba9c4014168ad
Bilal Bas <bilal.bas@gmx.de>
2023-12-19 08:34:26 +0100

    FeedImageDataLoaderWithFallbackComposite.init does not load image data

---
+++ EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,51 @@
+import XCTest
+import EssentialFeed
+
+final class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
+    
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+        
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return Task()
+    }
+    
+    private struct Task: FeedImageDataLoaderTask {
+        func cancel () { }
+    }
+
+}
+
+final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+
+    func test_init_doesNotLoadImageData() {
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        _ = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
+    }
+    
+    // MARK: - Helpers
+    
+    private class LoaderSpy: FeedImageDataLoader {
+        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> EssentialFeed.FeedImageDataLoaderTask {
+            messages.append((url, completion))
+            
+            return Task()
+        }
+        
+        private struct Task: FeedImageDataLoaderTask {
+            func cancel() { }
+        }
+        
+    }
+}
