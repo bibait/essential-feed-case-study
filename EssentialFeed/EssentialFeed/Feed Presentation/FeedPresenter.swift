b6fd45d430f4cc563d324bf06be1e9acaab86e3e
Bilal Bas <bilal.bas@gmx.de>
2023-12-23 10:34:12 +0100

    Remove unused FeedPresenter logic

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -1,44 +1,11 @@
 import Foundation
 
-public protocol FeedView {
-    func display(_ viewModel: FeedViewModel)
-}
-
 public final class FeedPresenter {
-    private let errorView: ResourceErrorView
-    private let loadingView: ResourceLoadingView
-    private let feedView: FeedView
-    
     public static var title: String {
-        return NSLocalizedString("FEED_VIEW_TITLE", tableName: "Feed", bundle: Bundle(for: FeedPresenter.self), comment: "Title for the feed view")
-    }
-    
-    private var feedLoadError: String {
-        return NSLocalizedString("GENERIC_CONNECTION_ERROR",
-                                 tableName: "Shared",
-                                 bundle: Bundle(for: FeedPresenter.self),
-                                 comment: "Error message displayed when we can't load the image feed from the server")
-    }
-    
-    public init(errorView: ResourceErrorView, loadingView: ResourceLoadingView, feedView: FeedView) {
-        self.errorView = errorView
-        self.loadingView = loadingView
-        self.feedView = feedView
-    }
-    
-    public func didStartLoadingFeed() {
-        errorView.display(.noError)
-        loadingView.display(ResourceLoadingViewModel(isLoading: true))
-    }
-    
-    public func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(Self.map(feed))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
-    }
-    
-    public func didFinishLoading(with error: Error) {
-        errorView.display(ResourceErrorViewModel.error(message: feedLoadError))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
+        NSLocalizedString("FEED_VIEW_TITLE",
+                          tableName: "Feed",
+                          bundle: Bundle(for: FeedPresenter.self),
+                          comment: "Title for the feed view")
     }
     
     public static func map(_ feed: [FeedImage]) -> FeedViewModel {
