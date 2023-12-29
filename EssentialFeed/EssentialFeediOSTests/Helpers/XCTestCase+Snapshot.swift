d1bb73a50cccc6c531b55310c5bbe8dffc0b7137
Bilal Bas <bilal.bas@gmx.de>
2023-12-29 16:48:20 +0100

    Move snapshot helpers to helper class

---
+++ EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2023 Wave Innovation Studio. All rights reserved.
+//
+
+import XCTest
+
+final class XCTestCase_Snapshot: XCTestCase {
+
+    func test() {
+        XCTFail("No tests implemented yet.")
+    }
+    
+}
