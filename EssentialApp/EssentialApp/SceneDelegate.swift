5fed61bbde8945af28a1ab1996208085d23a4a6b
Bilal Bas <bilal.bas@gmx.de>
2024-01-08 18:24:07 +0100

    Add 'assertionFailure' to catch CoreData issues in Debug builds

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -17,6 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     .defaultDirectoryURL()
                     .appending(path: "feed-store.sqlite"))
         } catch {
+            assertionFailure("Failed to instantiate CoreData store with error: \(error.localizedDescription)")
             return NullStore()
         }
     }()
